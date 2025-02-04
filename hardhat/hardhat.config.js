e42740073a667917944a4247288446fad5743ec1
scottDev25 <scott.macdonald2023@outlook.com>
2025-02-04 22:45:54 +1030

    init hardhat

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
+}
